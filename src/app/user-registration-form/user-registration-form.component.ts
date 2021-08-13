fac798d123368d0228e5b0110112f7bfe64924e2
pstaal <peter.staal@gmail.com>
2021-08-13 15:46:07 +0200

    add console.log

--- src/app/user-registration-form/user-registration-form.component.ts
+++ src/app/user-registration-form/user-registration-form.component.ts
@@ -33,6 +33,7 @@ registerUser(): void {
     this.fetchApiData.userRegistration(this.userData).subscribe((result) => {
   // Logic for a successful user registration goes here! (To be implemented)
      this.dialogRef.close(); // This will close the modal on success!
+     console.log(response)
      this.snackBar.open(result, 'OK', {
         duration: 2000
      });
