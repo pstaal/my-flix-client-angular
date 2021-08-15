fd9d4e56b9dbfa84bca9cac1036a4c6678f36d5e
pstaal <peter.staal@gmail.com>
2021-08-15 11:16:54 +0200

    set localstorage after login

--- src/app/user-login-form/user-login-form.component.ts
+++ src/app/user-login-form/user-login-form.component.ts
@@ -31,6 +31,8 @@ ngOnInit(): void {
 loginUser(): void {
     this.fetchApiData.userLogin(this.userData).subscribe((result) => {
   // Logic for a successful user registration goes here! (To be implemented)
+      localStorage.setItem('username', result.user.Username);
+      localStorage.setItem('token', result.token);
      this.dialogRef.close(); // This will close the modal on success!
      this.snackBar.open(result, 'OK', {
         duration: 2000
