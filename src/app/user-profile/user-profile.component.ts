2e056bb028f57278f069ff5c412fa8ac008e1a75
pstaal <peter.staal@gmail.com>
2021-08-16 13:52:51 +0200

    debugging

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -1,14 +1,10 @@
 
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
-// You'll use this import to close the dialog on success
-import { MatDialogRef } from '@angular/material/dialog';
 
 // This import brings in the API calls we created in 6.2
 import { UserRegistrationService } from '../fetch-api-data.service';
 
-// This import is used to display notifications back to the user
-import { MatSnackBar } from '@angular/material/snack-bar';
 
 
 @Component({
@@ -40,16 +36,7 @@ getUser(): void {
 editUser(): void {
     this.fetchApiData.editUser(this.user).subscribe((result) => {
   // Logic for a successful user registration goes here! (To be implemented)
-     this.dialogRef.close(); // This will close the modal on success!
-     
-     this.snackBar.open(result, 'OK', {
-        duration: 2000
-     });
-    }, (result) => {
-      this.snackBar.open(result, 'OK', {
-        duration: 2000
-      });
-    });
+     console.log(result);
   }
 
   }
